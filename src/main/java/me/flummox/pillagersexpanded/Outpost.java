9ef7fcca5463f6b521b5ce2973a0b90bd3c455ed
Declan <declanbarre77@gmail.com>
2022-01-06 01:05:43 +1000

    Outpost extensions complete

--- src/main/java/me/flummox/pillagersexpanded/Outpost.java
+++ src/main/java/me/flummox/pillagersexpanded/Outpost.java
@@ -5,6 +5,7 @@ import me.flummox.pillagersexpanded.eventHandlers.moveRunnable;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.entity.EntityType;
 
@@ -24,6 +25,57 @@ public class Outpost {
 
     boolean hasWall;
 
+    Material[][][] building = {
+            {       {Material.DARK_OAK_LOG, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.DARK_OAK_LOG},
+                    {Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS},
+                    {Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS},
+                    {Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS},
+                    {Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS},
+                    {Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS},
+                    {Material.DARK_OAK_LOG, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.DARK_OAK_LOG}},
+
+            {       {Material.DARK_OAK_LOG, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, null, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, Material.DARK_OAK_LOG},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {null, null, null, null, null, null, null},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {Material.DARK_OAK_LOG, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, null, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, Material.DARK_OAK_LOG}},
+
+            {       {Material.DARK_OAK_LOG, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, null, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, Material.DARK_OAK_LOG},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {null, null, null, null, null, null, null},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {Material.DARK_OAK_LOG, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, null, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, Material.DARK_OAK_LOG}},
+
+            {       {Material.DARK_OAK_LOG, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, Material.DARK_OAK_LOG},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {Material.DARK_OAK_PLANKS, null, null, null, null, null, Material.DARK_OAK_PLANKS},
+                    {Material.DARK_OAK_LOG, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, Material.DARK_OAK_PLANKS, Material.DARK_OAK_LOG}},
+
+            {       {Material.DARK_OAK_LOG, Material.COBBLESTONE, Material.COBBLESTONE, Material.COBBLESTONE, Material.COBBLESTONE, Material.COBBLESTONE, Material.DARK_OAK_LOG},
+                    {Material.COBBLESTONE, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.COBBLESTONE},
+                    {Material.COBBLESTONE, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.COBBLESTONE},
+                    {Material.COBBLESTONE, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, null, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.COBBLESTONE},
+                    {Material.COBBLESTONE, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.COBBLESTONE},
+                    {Material.COBBLESTONE, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.BIRCH_PLANKS, Material.COBBLESTONE},
+                    {Material.DARK_OAK_LOG, Material.COBBLESTONE, Material.COBBLESTONE, Material.COBBLESTONE, Material.COBBLESTONE, Material.COBBLESTONE, Material.DARK_OAK_LOG}},
+
+            {       {Material.DARK_OAK_LOG, Material.DARK_OAK_FENCE, Material.DARK_OAK_FENCE, Material.DARK_OAK_FENCE, Material.DARK_OAK_FENCE, Material.DARK_OAK_FENCE, Material.DARK_OAK_LOG},
+                    {Material.DARK_OAK_FENCE, null, null, null, null, null, Material.DARK_OAK_FENCE},
+                    {Material.DARK_OAK_FENCE, null, null, null, null, null, Material.DARK_OAK_FENCE},
+                    {Material.DARK_OAK_FENCE, null, null, null, null, null, Material.DARK_OAK_FENCE},
+                    {Material.DARK_OAK_FENCE, null, null, null, null, null, Material.DARK_OAK_FENCE},
+                    {Material.DARK_OAK_FENCE, null, null, null, null, null, Material.DARK_OAK_FENCE},
+                    {Material.DARK_OAK_LOG, Material.DARK_OAK_FENCE, Material.DARK_OAK_FENCE, Material.DARK_OAK_FENCE, Material.DARK_OAK_FENCE, Material.DARK_OAK_FENCE, Material.DARK_OAK_LOG}},
+
+    };
+
     /**
      * Create a digital outpost
      * @param outpostID
@@ -40,16 +92,31 @@ public class Outpost {
         outpostLocation.setZ(outpostLocation.getBlockZ() );
         outpostLocation.setY(world.getHighestBlockYAt(outpostLocation.getBlockX(), outpostLocation.getBlockZ() ));
         if (locationMaterial != Material.SPAWNER) {
-            outpostLocation.getBlock().setType(Material.SPAWNER);
-            ((CreatureSpawner) outpostLocation.getBlock()).setSpawnedType(EntityType.VINDICATOR);
+            Block block = outpostLocation.getBlock();
+
+            block.setType(Material.SPAWNER);
+            CreatureSpawner creatureSpawner = (CreatureSpawner) block.getState();
+            creatureSpawner.setSpawnedType(EntityType.VINDICATOR);
         } else {
             System.out.println(" " + locationMaterial);
         }
         this.outpostLocation = outpostLocation;
         this.level = 1;
         this.outpostID = outpostID;
-        new buildOutpostRunnable(outpostLocation, Main.getPlugin(Main.class)).runTaskAsynchronously(Main.getPlugin(Main.class));
-        generateWall();
+        //new buildOutpostRunnable(outpostLocation, Main.getPlugin(Main.class)).runTask(Main.getPlugin(Main.class));
+        int offset = 3;
+        for (int y = 0; y < building.length; y++) {
+            for (int x = 0; x < building[y].length; x++) {
+                for (int z = 0; z < building[y][x].length; z++) {
+                    //Spawn the specified block at the location
+                    System.out.println("y: " + y + " x: " + x + " z: " + z);
+                    if (building[y][x][z] != null) {
+                        world.getBlockAt(new Location(world,outpostLocation.getX() - offset + x,outpostLocation.getY() + y, outpostLocation.getZ() - offset + z)).setType(building[y][x][z]);
+                    }
+                }
+            }
+        }
+        generateWall(world);
         System.out.println("CREATED " + outpostID + "| x: " + outpostLocation.getX() + " y: " + outpostLocation.getY() + " z: " + outpostLocation.getZ());
         save();
     }
@@ -68,7 +135,6 @@ public class Outpost {
         this.hasWall = DataManager.getInstance().getConfig().getBoolean(outpostString + "hasWall");
         this.outpostLocation = new Location(getServer().getWorld("world"), currentX, currentY, currentZ);
         System.out.println("LOADED " + outpostID + "| x: " + outpostLocation.getX() + " y: " + outpostLocation.getY() + " z: " + outpostLocation.getZ());
-        generateWall();
     }
 
     public void upgradeOutpost() {
@@ -76,7 +142,16 @@ public class Outpost {
 
     }
 
-    private void generateWall() {
+    private void generateWall(World world) {
+        int squareOffset = 40;
+        int yOffset = 20;
+        for (int x = 0; x < 80; x++) {
+            for (int y = 0; y < 40; y++) {
+                for (int z = 0; z < 80; z++) {
+                    world.getBlockAt(new Location(world,outpostLocation.getX() - squareOffset + x,outpostLocation.getY() - yOffset + y, outpostLocation.getZ() - squareOffset + z)).setType(building[y][x][z]);
+                }
+            }
+        }
         //if (hasWall) {
             //outpostLocation.getBlock().setType(Material.BIRCH_PLANKS);
            // hasWall = true;
